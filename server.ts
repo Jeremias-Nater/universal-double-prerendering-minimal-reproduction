559ce452d33ebd66437eb70b54a7980be177a495
jeremias <jeremias@in-temp.ch>
2021-12-14 14:18:26 +0100

    minimal reproduction ssr time

--- server.ts
+++ server.ts
@@ -7,35 +7,43 @@ import { join } from 'path';
 import { AppServerModule } from './src/main.server';
 import { APP_BASE_HREF } from '@angular/common';
 import { existsSync } from 'fs';
+import { performance } from 'perf_hooks';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app() {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const distFolder = join( process.cwd(), 'dist/browser' );
+  const indexHtml = existsSync( join( distFolder, 'index.original.html' ) ) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
-  server.engine('html', ngExpressEngine({
+  server.engine( 'html', ngExpressEngine( {
     bootstrap: AppServerModule,
-  }));
+  } ) );
 
-  server.set('view engine', 'html');
-  server.set('views', distFolder);
+  server.set( 'view engine', 'html' );
+  server.set( 'views', distFolder );
 
   // TODO: implement data requests securely
-  server.get('/api/**', (req, res) => {
-    res.status(404).send('data requests are not yet supported');
-  });
+  server.get( '/api/**', ( req, res ) => {
+    res.status( 404 ).send( 'data requests are not yet supported' );
+  } );
 
   // Serve static files from /browser
-  server.get('*.*', express.static(distFolder, {
-    maxAge: '1y'
-  }));
+  server.get( '*.*', express.static( distFolder, {
+    maxAge: '1y',
+  } ) );
 
   // All regular routes use the Universal engine
-  server.get('*', (req, res) => {
-    res.render(indexHtml, { req, providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }] });
-  });
+  server.get( '*', ( req, res ) => {
+    const start = performance.now();
+    res.render( indexHtml, {
+      req,
+      providers: [{ provide: APP_BASE_HREF, useValue: req.baseUrl }],
+    }, ( error, html ) => {
+      res.send( html );
+      console.log( ' total ssr duration: ', performance.now() - start );
+    } );
+  } );
 
   return server;
 }
@@ -45,9 +53,9 @@ function run() {
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+  server.listen( port, () => {
+    console.log( `Node Express server listening on http://localhost:${port}` );
+  } );
 }
 
 // Webpack will replace 'require' with '__webpack_require__'
@@ -56,7 +64,7 @@ function run() {
 declare const __non_webpack_require__: NodeRequire;
 const mainModule = __non_webpack_require__.main;
 const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+if ( moduleFilename === __filename || moduleFilename.includes( 'iisnode' ) ) {
   run();
 }
 
