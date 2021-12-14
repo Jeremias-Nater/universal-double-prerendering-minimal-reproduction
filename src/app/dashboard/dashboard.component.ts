2da699650ce3b3007f113ddc3f0aac587a5e8f74
jeremias <jeremias@in-temp.ch>
2021-12-14 16:00:01 +0100

    ssr time debug

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,22 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
-@Component({
+@Component( {
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
-})
+  styleUrls: ['./dashboard.component.css'],
+} )
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor( private heroService: HeroService ) {
+  }
 
   ngOnInit() {
     this.getHeroes();
   }
 
   getHeroes(): void {
+    // this.heroes = [
+    //   {
+    //     'id': 12,
+    //     'name': 'Narco',
+    //   },
+    //   {
+    //     'id': 13,
+    //     'name': 'Bombasto',
+    //   },
+    //   {
+    //     'id': 14,
+    //     'name': 'Celeritas',
+    //   },
+    //   {
+    //     'id': 15,
+    //     'name': 'Magneta',
+    //   },
+    // ];
+    const start = performance.now();
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe( heroes => {
+        console.log( 'hi?' );
+        this.heroes = heroes.slice( 1, 5 );
+        console.log( this.heroes );
+        console.log( 'request took: ', performance.now() - start );
+      } );
   }
 }
